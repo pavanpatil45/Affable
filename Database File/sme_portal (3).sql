446b4bbb09a927a3a5acea6ac359f7bd554deb5a
P4V4N <pavanadhao685@gmail.com>
2021-02-03 18:52:54 +0530

    db

--- Database File/sme_portal (3).sql
+++ Database File/sme_portal (3).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 03, 2021 at 02:19 PM
+-- Generation Time: Feb 03, 2021 at 02:22 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.9
 
